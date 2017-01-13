0f80625a5326eacafc2270d286936327882d296e
Calvin <crutley@cisco.com>
2017-01-13 13:42:21 -0800

    Debuging Stage One

--- openstack_dashboard/dashboards/testDash/static/testDash/testPan/table/output.controller.js
+++ openstack_dashboard/dashboards/testDash/static/testDash/testPan/table/output.controller.js
@@ -1,15 +1,18 @@
-
+/**
+ * Created by lbaerisw on 12/17/16.
+ */
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('horizon.dashboard.testDash.output')
-    .controller('OutController', OutController);
+    angular
+        .module('horizon.dashboard.testDash.output')
+        .controller('OutController', OutController);
 
-  function OutController() {
-    var ctrl = this;
+    function OutController() {
 
-      ctrl.eventsTable = [
+        var ctrl = this;
+        //table of events, needs to be ordered
+        ctrl.eventsTable = [
             {
                 name:"instance1",
                 body:"Blown up!"
@@ -20,36 +23,71 @@
             } //example objects
         ];
 
-        this.updateTable = function(event) {
-            eventsTable.push({name: 'newEvent.name',event:'newEvent.event'});
+        ctrl.updateTable = function(newEvent) {
+            ctrl.eventsTable.push({name: newEvent.name, body: newEvent.body});
         };
 
-        var wsroute = 'localhost'; //to allow for reuse, should just use OPENSTACK_HOST value? If empty use localhost?
-        var connection = 'ws://' + wsroute + ':80'; //should be port 80 by default? or port 443 if secure connection?
+        ctrl.opensuccess = 0;
+        ctrl.connclosed = 0;
+        ctrl.authString = '';
+        ctrl.receivedString = '';
+
+        ctrl.success = function() {
+            ctrl.opensuccess += 1;
+        }
+
+        ctrl.close = function() {
+            ctrl.connclosed += 1;
+        }
+
+        ctrl.createString = function(string) {
+            ctrl.authString = string;
+        }
+
+        ctrl.createReceivedString = function(nstring) {
+            ctrl.receivedString = nstring;
+        }
+
+
+        var wsroute = '172.29.86.71'; //need to dynamically allocate
+        var connection = 'ws://' + wsroute + ':9000'; //need to dynamically allocate
         var ws = new WebSocket(connection);
 
         //should these events be in the factory, or should the factory simply create the connection and return the WebSocket object?
         //called when connection is opened
         ws.onopen = function() {
-            var client_id = 'placeholder'; //need to figure out how to dynamically get
-            var project_id = 'placeholder'; //need to figure out how to dynamically get
+            ctrl.success();
+            var client_id = 'a050e8fce5db46de98b557066a43c201'; //need to figure out how to dynamically get
+            var project_id = 'c3ca2ccaeafa4267a84cc0164e66c874'; //replace as needed
+            var authtoken = 'gAAAAABYeRxwxIMhmOGQw_Wb8W7E0IuBePBOT3gdFlib1t3PEwkjQS7F-qVKHDomVvAxn5CQi6MnakrHBBrOnaW3UxcVSamAS2W0NuykUqjIxmfsBAfed31jphuueE1SHBRWRTFmsxElZgdNTSbJiwi1VoZkx0NBGD9gZw8w_1j9uMegKZf57JI';
 
-            authenticate = function() {
-                ws.send(authentication); //placeholder
+            ctrl.success();
+            authenticate();
+            ctrl.success();
+            subscribe();
+            ctrl.success();
+
+            function authenticate() {
+                var authentication = {'action': 'authenticate',
+                    'headers': {'X-Auth-Token': authtoken, 'Client-ID': client_id, 'X-Project-ID': project_id}};
+
+
+                //console.log(JSON.stringify(authentication));
+                ctrl.createString(JSON.stringify(authentication));
+                ws.send(JSON.stringify(authentication));
+
             };
 
-            subscribe = function() { //somehow have a parameter that allows to set different subscriptions? not priority
-                var subscription = {'action': subscription_create,
-                                    'headers': {'Client-ID': placeholder, 'X-Project-ID': placeholder},
-                                    'body': {'queue_name': 'horizon_events', 'ttl': 3600}};
+            function subscribe() { //somehow have a parameter that allows to set different subscriptions? not priority
+                var subscription = {'action': 'subscription_create',
+                    'headers': {'Client-ID': client_id, 'X-Project-ID': project_id},
+                    'body': {'queue_name': 'horizon_events_test', 'ttl': 3600}}; //could dynamically assign queue name
                 ws.send(JSON.stringify(subscription));
             };
-
-            subscribe;
-
         };
 
         ws.onclose = function() {
+            ctrl.close();
             console.log('connection closed'); //need to handle connection close?
         };
 
@@ -59,16 +97,10 @@
 
         ws.onmessage = function(event) {
             var newEvent = JSON.parse(event.data);
-            updateTable(newEvent);
+            ctrl.createReceivedString(newEvent);
+            ctrl.updateTable(newEvent.body);
         };
 
-
-      ctrl.fruits = [
-            { id: 4019, name: "apple", inStock: 75},
-            { id: 4232, name: "banana", inStock: 120},
-            { id: 4610, name: "cantaloupe", inStock: 98},
-            { id: 4872, name: "durian", inStock: 4}
-        ];
-  }
+    }
 
 })();
