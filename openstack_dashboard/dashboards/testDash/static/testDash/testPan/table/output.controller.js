f1a036d8a9a1c40fe741c6602e28eface10ca80d
Laurent Baeriswyl <lbaerisw@cisco.com>
2017-02-02 15:52:22 -0800

    Code Cleanup pt2
    Moved response handling out of onmessage event and into its own function for cleaner code.

--- openstack_dashboard/dashboards/testDash/static/testDash/testPan/table/output.controller.js
+++ openstack_dashboard/dashboards/testDash/static/testDash/testPan/table/output.controller.js
@@ -48,6 +48,29 @@
             ctrl.authString = string;
         }
 
+        ctrl.responseHandler = function(response) {
+            if(response.Message_Type == "Notification") {
+                console.log(response.body);
+                ctrl.updateTable(response.body);
+            }
+
+            else {
+                var message = response.request.action;
+                message += ': ';
+
+                if(response.body.hasOwnProperty("message")) {
+                    message += response.body.message;
+                    console.log(message);
+                    toast.add('success', message);
+                }
+                else {
+                    message += response.body.error;
+                    console.log(message);
+                    toast.add('error', message);
+                }
+            }
+        }
+
         //websocket stuff
         var wsroute = '172.29.86.71'; //need to dynamically allocate
         var connection = 'ws://' + wsroute + ':9000'; //need to dynamically allocate
@@ -106,26 +129,8 @@
             //parse contents
             var response = JSON.parse(event.data);
 
-            //This entire bit will move into its own function for cleaner code
-            if(response.Message_Type == "Notification") {
-                console.log(response.body);
-                ctrl.updateTable(response.body);
-            }
+            ctrl.responseHandler(response);
 
-            else {
-                var message = response.request.action;
-                message += ': ';
-                if(response.body.hasOwnProperty("message")) {
-                    message += response.body.message;
-                    console.log(message);
-                    toast.add('success', message);
-                }
-                else {
-                    message += response.body.error;
-                    console.log(message);
-                    toast.add('error', message);
-                }
-            }
         };
 
     }
