3a612fef01395327082c299e6df0d81894e0dde1
Calvin <crutley@cisco.com>
2017-02-08 16:51:19 -0800

    Automatically Authenticates with zaqar.

--- openstack_dashboard/dashboards/testDash/static/testDash/testPan/table/output.controller.js
+++ openstack_dashboard/dashboards/testDash/static/testDash/testPan/table/output.controller.js
@@ -9,10 +9,16 @@
         .controller('OutController', OutController);
 
     OutController.$inject = [
-        'horizon.framework.widgets.toast.service'
+        '$q',
+        'horizon.framework.widgets.toast.service',
+        'horizon.app.core.images.resourceType',
+        'horizon.framework.conf.resource-type-registry.service',
+        'horizon.app.core.openstack-service-api.userSession',
+        'horizon.framework.util.uuid.service'
     ];
 
-    function OutController(toast) {
+    function OutController($q, toast, imageResourceTypeCode, registry, userSession) {
+
 
         var ctrl = this;
         //table of events, needs to be ordered
@@ -32,9 +38,34 @@
         };
 
         //testing variables and functions
+        var session =  userSession.get();
         ctrl.opensuccess = 0;
         ctrl.msgeventtrigger = 0;
         ctrl.authString = '';
+        ctrl.projectId = '';
+        ctrl.uuid = '';
+        ctrl.token = '';
+
+
+        ctrl.resourceType = registry.getResourceType(imageResourceTypeCode);
+
+        /* Methods are not needed as some code below does it, but leaving there as a reference as this might be the
+        best way to do it, will need to check.
+
+        function setProject(session) {
+            ctrl.projectId = session.project_id;
+        }
+
+        function setToken(session) {
+            ctrl.token = session.token;
+        }
+
+        function setUser(session) {
+            ctrl.uuid = session.id;
+        }
+
+        */
+
 
         ctrl.success = function() {
             ctrl.opensuccess += 1;
@@ -79,31 +110,45 @@
         //should these events be in the factory, or should the factory simply create the connection and return the WebSocket object?
         //called when connection is opened
         ws.onopen = function() {
-            ctrl.success();
-            var client_id = '7254b2c6-a30c-478d-8b12-46bf71fbc41e'; //need to figure out how to dynamically get
-            var project_id = 'c3ca2ccaeafa4267a84cc0164e66c874'; //replace as needed
-            var authtoken = 'gAAAAABYk65lk9WUV1wCvvTwAI_SvTIEq2PUxmo-as7VMpfCg2vLyoZfP2mJXjn1dgYYphnieSB2mL8wCGp-DzWM5Gwr9v8CnvTVSeA3gzFrpx5MXe2nSDj6RzlZxp730hAm1XWhMDcrTd9rg3M00EfVpk4BQ4tIWNELHbqfboOOCjJsz0-a3ls';
 
-            ctrl.success();
-            authenticate();
-            ctrl.success();
-            subscribe();
-            ctrl.success();
+            $q.all((userSession.get()).then(function(data) {
 
-            function authenticate() {
-                var authentication = {'action': 'authenticate',
-                    'headers': {'X-Auth-Token': authtoken, 'Client-ID': client_id, 'X-Project-ID': project_id}};
-                ws.send(JSON.stringify(authentication));
+                ctrl.projectId = data.project_id;
+                ctrl.uuid = data.id;
+                ctrl.token = data.token;
 
-            };
+                ctrl.success();
+                //var client_id = '7254b2c6-a30c-478d-8b12-46bf71fbc41e'; //need to figure out how to dynamically get
+                var client_id = ctrl.uuid;
+                //var project_id = 'c3ca2ccaeafa4267a84cc0164e66c874'; //replace as needed
+                var project_id = ctrl.projectId;
+                //var authtoken = 'gAAAAABYk65lk9WUV1wCvvTwAI_SvTIEq2PUxmo-as7VMpfCg2vLyoZfP2mJXjn1dgYYphnieSB2mL8wCGp-DzWM5Gwr9v8CnvTVSeA3gzFrpx5MXe2nSDj6RzlZxp730hAm1XWhMDcrTd9rg3M00EfVpk4BQ4tIWNELHbqfboOOCjJsz0-a3ls';
+                var authtoken = ctrl.token;
 
-            function subscribe() { //somehow have a parameter that allows to set different subscriptions? not priority
-                var subscription = {'action': 'subscription_create',
-                    'headers': {'Client-ID': client_id, 'X-Project-ID': project_id},
-                    'body': {'queue_name': 'horizon_events_test', 'ttl': 3600}}; //could dynamically assign queue name
-                ws.send(JSON.stringify(subscription));
-            };
+                ctrl.success();
+                authenticate();
+                ctrl.success();
+                subscribe();
+                ctrl.success();
+
+                function authenticate() {
+                    var authentication = {'action': 'authenticate',
+                        'headers': {'X-Auth-Token': authtoken, 'Client-ID': client_id, 'X-Project-ID': project_id}};
+                    ws.send(JSON.stringify(authentication));
+                    console.log(JSON.stringify(authentication));
+
+                }
+
+                function subscribe() { //somehow have a parameter that allows to set different subscriptions? not priority
+                    var subscription = {'action': 'subscription_create',
+                        'headers': {'Client-ID': client_id, 'X-Project-ID': project_id},
+                        'body': {'queue_name': 'horizon_events_test', 'ttl': 3600}}; //could dynamically assign queue name
+                    ws.send(JSON.stringify(subscription));
+                }
+
+            }));
         };
+
 
         ws.onclose = function(event) {
             console.log(event);
