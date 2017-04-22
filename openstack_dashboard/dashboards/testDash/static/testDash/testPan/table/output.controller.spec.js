df578f25a5830afab130b75b1daed795bcdc32a3
Laurent Baeriswyl <lbaerisw@cisco.com>
2017-04-22 13:19:23 -0700

    Added test files & stubs to be filled

---
+++ openstack_dashboard/dashboards/testDash/static/testDash/testPan/table/output.controller.spec.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+
+  describe('horizon.app.core.images', function () {
+    it('should exist', function () {
+      expect(angular.module('horizon.app.core.images')).toBeDefined();
+    });
+  });
+})();
